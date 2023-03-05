ba2dddbf579fc56182d377b9124e9dc69b50572e
volodymyrlogika <95148713+volodymyrlogika@users.noreply.github.com>
2023-03-05 12:51:15 +0200

    lesson 2

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) # Створюємо веб–додаток Flask
 
 @app.route("/") # Вказуємо url-адресу для виклику функції
 def index():
-    return "Hello, World!" #Результат, що повертається у браузер
+    return render_template("index.html")#Результат, що повертається у браузер
 
 if __name__ == "__main__":
-    app.run() # Запускаємо веб-сервер з цього файлу
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(port=5001, debug=True) # Запускаємо веб-сервер з цього файлу
